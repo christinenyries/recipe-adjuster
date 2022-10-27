acbc5b7147f92ffdb0f2668277bcf53a84bcef5c
Christine Nyries Yambao <yambao.cn@gmail.com>
2022-10-27 11:12:29 +0800

    feat: added vuetify and a header

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar app :class="appBarClasses">
+    <v-toolbar-title>Recipe Adjuster</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn :class="appBarBtnClasses">Home</v-btn>
+      <v-btn :class="appBarBtnClasses">Recipes</v-btn>
+      <v-btn :class="appBarBtnClasses">About</v-btn>
+      <v-btn :class="appBarBtnClasses">Login</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      appBarClasses: ["elevation-0"],
+      appBarBtnClasses: ["elevation-0"],
+    };
+  },
+};
+</script>
